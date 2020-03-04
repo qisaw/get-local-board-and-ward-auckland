c2a64ac6dbc5f34d8ecafc7e73cb624f4406e204
Wasiq Kashkari <w.kashkari@outlook.com>
2020-03-04 18:17:03 +1300

    Add selenium integration which is much faster

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
 const { Parser } = require("json2csv");
 const data = require("../../data.json");
+const csvToJSON = require("csvtojson");
 
-const delay = ms =>
-  new Promise(res => setTimeout(() => console.log("res run") || res(), ms));
+const delay = ms => new Promise(res => setTimeout(() => res(), ms));
 const getWandAndBoard = async (address, suburb) => {
   let results;
   let _res;
@@ -12,22 +12,33 @@ const getWandAndBoard = async (address, suburb) => {
   });
   cy.visit(
     "https://www.aucklandcouncil.govt.nz/about-auckland-council/how-auckland-council-works/governing-body-wards-committees/wards/Pages/find-your-ward.aspx"
-  );
-  cy.get(
-    "#ctl00_SPWebPartManager1_g_edfffc2b_c31b_465c_90ea_b46875a5d96e_ctl00_ctl00_ACAutoComplete"
-  ).type(`${address} ${suburb}`);
+  ).then(() => {
+    return cy
+      .get(
+        "#ctl00_SPWebPartManager1_g_edfffc2b_c31b_465c_90ea_b46875a5d96e_ctl00_ctl00_ACAutoComplete"
+      )
+      .type(`${address} ${suburb}`)
+      .then(() => {
+        return delay(500);
+      });
+  });
   cy.get(".dropdown-item")
     .first()
     .click()
-    .then(() => {
-      const board = Cypress.$("#FindYourResponse h4 b")
-        .eq(1)
-        .text()
-        .trim();
-      const ward = Cypress.$("#FindYourResponse h4 b")
-        .eq(2)
-        .text()
-        .trim();
+    .then(async () => {
+      let board = "";
+      let ward = "";
+      while (!board && !ward) {
+        board = Cypress.$("#FindYourResponse h4 b")
+          .eq(1)
+          .text()
+          .trim();
+        ward = Cypress.$("#FindYourResponse h4 b")
+          .eq(2)
+          .text()
+          .trim();
+        await delay(100);
+      }
 
       results = {
         address,
@@ -35,36 +46,52 @@ const getWandAndBoard = async (address, suburb) => {
         ward,
         board
       };
+
       _res();
     });
   await promise;
   return results;
 };
 context("Actions", () => {
-  let results;
-  beforeEach(() => {
-    results = [];
-  });
-  after(async () => {
-    const fields = ["address", "suburb", "board", "ward"];
-    const opts = { fields };
-    const parser = new Parser(opts);
-    console.log(results);
-    const csv = parser.parse(results);
-    console.log(csv);
-    cy.writeFile("output.csv", csv, "utf8");
-  });
-
   // https://on.cypress.io/interacting-with-elements
+  let initailData = [];
+  beforeEach(async () => {
+    let _res;
+    const promise = new Promise(res => (_res = res));
+    await cy.readFile("output.csv").then(async csvString => {
+      initailData = await csvToJSON().fromString(csvString);
+      _res();
+    });
+    await promise;
+  });
 
   it(".type() - type into a DOM element", async () => {
-    for (let i = 0; i < 3; i++) {
-      const r = await getWandAndBoard(
-        data[i]["Street Address"],
-        data[i]["Suburb"]
-      );
-      console.log("r is", r);
-      results.push(r);
+    for (let i = 0; i < data.length; i++) {
+      if (i % 20 === 0) {
+        cy.log(`entry number ${i}`);
+      }
+      const address = data[i]["Street Address"];
+      const suburb = data[i]["Suburb"];
+      let existingEntry = initailData.find(({ address: add, suburb: sub }) => {
+        return add === address && suburb === sub;
+      });
+      let result;
+      if (!existingEntry) {
+        result = await getWandAndBoard(address, suburb);
+      } else {
+        result = existingEntry;
+      }
+      const csv = `"${result.address}","${result.suburb}","${result.board}","${result.ward}"\n`;
+      if (i >= initailData.length) {
+        initailData.push(result);
+        cy.writeFile("output.csv", csv, { flag: "a+" });
+        if (initailData.length === data.length) {
+          break;
+        }
+        if (!existingEntry) {
+          throw new Error("continue");
+        }
+      }
     }
   });
 });
---
+++ selenium/index.js
@@ -0,0 +1,89 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const csvToJSON = require("csvtojson");
+const chrome = require("selenium-webdriver/chrome");
+const chromedriver = require("chromedriver");
+const fs = require("fs");
+const data = require("../data.json");
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+const getWandAndBoard = async (driver, address, suburb) => {
+  await driver.get(
+    "https://www.aucklandcouncil.govt.nz/about-auckland-council/how-auckland-council-works/governing-body-wards-committees/wards/Pages/find-your-ward.aspx"
+  );
+  driver.wait(
+    until.elementLocated(
+      By.id(
+        "ctl00_SPWebPartManager1_g_edfffc2b_c31b_465c_90ea_b46875a5d96e_ctl00_ctl00_ACAutoComplete"
+      )
+    )
+  );
+  const wb = driver.findElement(
+    By.id(
+      "ctl00_SPWebPartManager1_g_edfffc2b_c31b_465c_90ea_b46875a5d96e_ctl00_ctl00_ACAutoComplete"
+    )
+  );
+  const fullAddress = `${address} ${suburb}`;
+  driver.executeScript(`arguments[0].value='${fullAddress}';`, wb);
+  await driver
+    .findElement(
+      By.id(
+        "ctl00_SPWebPartManager1_g_edfffc2b_c31b_465c_90ea_b46875a5d96e_ctl00_ctl00_ACAutoComplete"
+      )
+    )
+    .sendKeys(Key.ENTER);
+  await driver.wait(until.elementIsEnabled(By.className("dropdown-item")));
+  await delay(200);
+  await driver.findElement(By.className("dropdown-item")).click();
+  await driver.wait(until.elementLocated(By.css("#FindYourResponse h4 b")));
+  const messages = await driver.findElements(By.css("#FindYourResponse h4 b"));
+  const [, boardElement, wardElement] = messages;
+  if (!boardElement || !wardElement) {
+    return {
+      address,
+      suburb,
+      board: "unknown",
+      ward: "unknown"
+    };
+  }
+  const [board, ward] = await Promise.all([
+    boardElement.getText(),
+    wardElement.getText()
+  ]);
+  return {
+    address,
+    suburb,
+    board,
+    ward
+  };
+};
+(async function example() {
+  let driver = await new Builder().forBrowser("chrome").build();
+  const initialDataAsString = fs.readFileSync("./output.csv", "utf8");
+  const initailData = await csvToJSON().fromString(initialDataAsString);
+  try {
+    for (let i = 0; i < data.length; i++) {
+      console.log(`entry number ${i}`);
+      const address = data[i]["Street Address"];
+      const suburb = data[i]["Suburb"];
+      console.log(`address ${address}, suburb ${suburb}`);
+      let existingEntry = initailData.find(({ address: add, suburb: sub }) => {
+        return add === address && suburb === sub;
+      });
+      let result;
+      if (!existingEntry) {
+        result = await getWandAndBoard(driver, address, suburb);
+      } else {
+        result = existingEntry;
+      }
+      const csv = `"${result.address}","${result.suburb}","${result.board}","${result.ward}"\n`;
+      if (i >= initailData.length) {
+        initailData.push(result);
+        fs.appendFileSync("./output.csv", csv);
+      }
+    }
+  } finally {
+    await driver.quit();
+  }
+})();
